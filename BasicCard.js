9492628355761650d250707f1e8b17eb386add35
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-21 14:16:48 -0400

    began trial question for test

--- BasicCard.js
+++ BasicCard.js
@@ -3,3 +3,13 @@ var inquirer = require("inquirer");
 var allQuestions = 0;
 var answerRight = 0;
 var answerWrong = 0;
+
+function ShowCard() {}
+
+
+var questions = [
+	{
+		text: 'George Washington was the first president of the United States.',
+
+
+}]
