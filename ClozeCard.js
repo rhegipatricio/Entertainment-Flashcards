67e2aac49dbb7a40d141067cab1208cd7b65f865
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-21 23:55:05 -0400

    changed missing answers to lower case to make it easier on the user

--- ClozeCard.js
+++ ClozeCard.js
@@ -6,7 +6,7 @@ var runapp = require('./runapp.js');
 var inquirer = require("inquirer");
 
 //var questions
-var questionOne = new runapp.clozecard("Blue French Horn", "... is what Ted gave to Robin after their first date.", "Blue French Horn is what Ted gave to Robin after their first date.");
+var questionOne = new runapp.clozecard("blue french horn", "... is what Ted gave to Robin after their first date.", "blue french horn is what Ted gave to Robin after their first date.");
 var questionTwo = new runapp.clozecard("three times", "Sheldon knocks ... when he goes to Penny's apartment", "Sheldon knocks three times when he goes to Penny's apartment");
 
 //pulls from inquirer to show questions to user
@@ -20,6 +20,7 @@ inquirer.prompt([{
 	message: questionTwo.missingText
 }
 
+//function that tells the user if they're right or wrong
 ]).then(function (inquirer) {
 
 	if (inquirer.answerOne === questionOne.clozeGone) {
