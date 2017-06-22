1e92bf719ba5d0eb650b2f9e9c37362c9bdf7997
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-21 21:01:26 -0400

    Cleaned up code on line 22

--- BasicCard.js
+++ BasicCard.js
@@ -10,3 +10,13 @@ var questionOne = new BasicCard(
 
 var questionTwo = new BasicCard(
 		"What team did Michael Jordan and the Chicago Bulls beat in Jordan's last ever NBA Finals?", "Utah Jazz");
+
+inquirer.prompt([{
+	type: 'input',
+	message: questionOne.front,
+	name: 'answerOne'
+}, {
+	type: 'input',
+	message: questionTwo.front,
+	name: 'answerTwo'
+}])
