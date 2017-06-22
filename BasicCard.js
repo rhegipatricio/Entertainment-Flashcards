e4be0a9bf7166feb35e1529bfa2d99c96ef4ec7e
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-21 22:16:22 -0400

    added the 4th question

--- BasicCard.js
+++ BasicCard.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 
+//shows the front and the back of the card
 var BasicCard = function(front, back) {
 	this.front = front;
 	this.back = back;
@@ -7,14 +8,18 @@ var BasicCard = function(front, back) {
 
 //questions
 var questionOne = new BasicCard(
-		"Who is the Logo of the NBA?", "Jerry West");
+	"Who is the Logo of the NBA?", "Jerry West");
 
 var questionTwo = new BasicCard(
-		"What team did Michael Jordan and the Chicago Bulls beat in Jordan's last ever NBA Finals?", "Utah Jazz");
+	"What team did Michael Jordan and the Chicago Bulls beat in Jordan's last ever NBA Finals?", "Utah Jazz");
 
 var questionThree = new BasicCard(
-		"What year did Kobe Bryant get drafted?", "1996");
+	"What year did Kobe Bryant get drafted?", "1996");
 
+var questionFour = new BasicCard(
+	"Although he was supremely successful in his career, Kobe Bryant did not get drafted first in his draft. What was the nickname of the player that DID get drafted first?", "The Answer");
+
+//pulls from inquirer to show questions to user
 inquirer.prompt([{
 	type: 'input',
 	name: 'answerOne',
@@ -27,13 +32,18 @@ inquirer.prompt([{
 	type: 'input',
 	name: 'answerThree',
 	message: questionThree.front
+}, {
+	type: 'input',
+	name: 'answerFour',
+	message: questionFour.front
 }
 
+//function that tells the user if they're right or wrong
 ]).then(function(inquirer) {
 	if (inquirer.answerOne === questionOne.back) {
 		console.log("It's Good!")
 	} else {
-		console.log('Oh he draws iron. No good!')
+		console.log('The iron is unkind. No good!')
 	}
 	if (inquirer.answerTwo === questionTwo.back) {
 		console.log('The Bulls have won!')
@@ -41,9 +51,14 @@ inquirer.prompt([{
 		console.log('Airball!')
 	}
 	if (inquirer.answerThree === questionThree.back) {
-		console.log('Right!')
+		console.log('Kobe with the win!')
 	} else {
-		console.log('Nope!')
+		console.log('Kobe has shot an airball!')
+	}
+	if (inquirer.answerFour === questionFour.back) {
+		console.log('Iverson for the win!')
+	} else {
+		console.log('A.I. with the turnover. The Sixers lose!')
 	}
 
 });
