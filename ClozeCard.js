0b0fd1f25a7a1e6dee42843212c21f4d3fed5aec
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-21 23:22:57 -0400

    added inquirer prompt in clozecard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -9,3 +9,23 @@ var inquirer = require("inquirer");
 var questionOne = new runapp.clozecard("Blue French Horn", "... is what Ted gave to Robin after their first date.", "Blue French Horn is what Ted gave to Robin after their first date.");
 var questionTwo = new runapp.clozecard("three times", "Sheldon knocks ... when he goes to Penny's apartment", "Sheldon knocks three times when he goes to Penny's apartment");
 
+//pulls from inquirer to show questions to user
+inquirer.prompt([{
+	type: 'input',
+	name: 'answerOne',
+	message: 'questionOne.missingText'
+}, {
+	type: 'input',
+	name: 'answerTwo',
+	message: 'questionTwo.missingText'
+}
+
+
+
+
+
+
+
+
+
+}])
