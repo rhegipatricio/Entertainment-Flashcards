8640700857ba912ebbdc008155d8b84f81884c5b
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-22 01:36:36 -0400

    adding code for final questions on cloze

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,6 +7,9 @@ var inquirer = require("inquirer");
 //var questions
 var questionOne = new runapp.clozecard("blue french horn", "... is what Ted gave to Robin after their first date.", "blue french horn is what Ted gave to Robin after their first date.");
 var questionTwo = new runapp.clozecard("three times", "Sheldon knocks ... when he goes to Penny's apartment", "Sheldon knocks three times when he goes to Penny's apartment");
+var questionThree = new runapp.clozecard("", " ... ", "");
+var questionFour = new runapp.clozecard("", " ... ", "");
+var questionFive = new runapp.clozecard("", " ... ", "");
 
 //pulls from inquirer to show questions to user
 inquirer.prompt([{
@@ -17,6 +20,10 @@ inquirer.prompt([{
 	type: 'input',
 	name: 'answerTwo',
 	message: questionTwo.missingText
+}, {
+	type: 'input',
+	name: 'answerThree',
+	message: questionThree.missingText
 }
 
 //function that tells the user if they're right or wrong
