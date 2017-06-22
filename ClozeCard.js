ab0623a38aa0325e689372a49adf4603d8a32dfc
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-22 01:58:13 -0400

    finished questions

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,9 +7,9 @@ var inquirer = require("inquirer");
 //var questions
 var questionOne = new runapp.clozecard("blue french horn", "... is what Ted gave to Robin after their first date.", "blue french horn is what Ted gave to Robin after their first date.");
 var questionTwo = new runapp.clozecard("three times", "Sheldon knocks ... when he goes to Penny's apartment", "Sheldon knocks three times when he goes to Penny's apartment");
-var questionThree = new runapp.clozecard("", " ... ", "");
-var questionFour = new runapp.clozecard("", " ... ", "");
-var questionFive = new runapp.clozecard("", " ... ", "");
+var questionThree = new runapp.clozecard("legendary", "Barney Stinson is famous for saying 'this is going to be ... '", "Barney Stinson is famous for saying 'this is going to be legendary'");
+var questionFour = new runapp.clozecard("The Rock", " ... is the stage and wrestling name for Dwayne Johnson when he wrestled in the WWE before he became a famous actor,", "The Rock is the stage and wrestling name for Dwayne Johnson when he wrestled for the WWE before he became a famous actor");
+var questionFive = new runapp.clozecard("feelings", "'No. I am your father' 'No! That's not true! That's impossible!' 'Search your ... . You know it to be true", "'No. I am your father' 'No! That's not true! That's impossible!' 'Search your feelings. You know it to be true'");
 
 //pulls from inquirer to show questions to user
 inquirer.prompt([{
